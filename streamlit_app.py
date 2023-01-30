34394e7e3d04f1589d7eedde18c9a936f1530e80
traas20 <42847284+traas20@users.noreply.github.com>
2023-01-30 14:28:29 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Pancakes')
+streamlit.text('French toast')
+streamlit.text('I guess something healthy')
